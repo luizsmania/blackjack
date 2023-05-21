f94d55450ee708c45580a0c33da50a6d0ddcd5b9
Luiz Smania <luizsmania1@gmail.com>
2023-05-21 16:58:53 +0000

    Function calculateTotal added, for calculating the points each round

--- run.py
+++ run.py
@@ -1,3 +1,36 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+# Setting Lists for the Deck, The player's Hand, and the Dealer's hand
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+'J', 'Q', 'K' 'A', 'J', 'Q', 'K' 'A', 'J', 'Q', 'K' 'A','J', 'Q', 'K' 'A',]
+playerHand = []
+dealerHand = []
+
+
+def dealCard(turn):
+    """
+    Deal Card function, selects a random card from the deck, appends it to
+    whoever turn it is, and removes this card from the deck
+    """
+    card = random.choice(deck)
+    turn.append(card)
+    deck.remove(card)
+
+def calculateTotal(turn):
+    """
+    This function calculates de total based on what card it is
+    """
+    total = 0
+    letters = ['J', 'K', 'Q']
+    for card in turn:
+        if card in range(1, 11):
+            total += card
+        elif card in letters:
+            total += 1
+        else:
+            if total > 11:
+                total += 1
+            else:
+                total += 11
+    return total
+
