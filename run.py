b4ba9beeed8e87bb9877e417661a93acea236e2d
Luiz Smania <66436972+luizsmania@users.noreply.github.com>
2023-06-21 20:57:55 +0000

    functions removed from inside of other functions. many changes made to comply with PEP8 rules

--- run.py
+++ run.py
@@ -1,160 +1,256 @@
-import sys,time,random
+"""
+    Blackjack Game
+"""
+
+import sys
+import time
+import random
 from os import system, name
 from time import sleep
-play = True
-#Sets players scores that will be increasing accordingly to wins
-dealerScore = 0 
-playerScore = 0
+
+PLAY = True
+# Sets players scores that will be increasing accordingly to wins
+
+DEALER_SCORE = 0
+PLAYER_SCORE = 0
+
+def generate_deck():
+    return [
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    "A",
+    "J",
+    "Q",
+    "K",
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    "A",
+    "J",
+    "Q",
+    "K",
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    "A",
+    "J",
+    "Q",
+    "K",
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    "A",
+    "J",
+    "Q",
+    "K",
+]
+
+deck = generate_deck()
+
+# Setting Lists for the Deck, The player's Hand, and the Dealer's hand
+playerHand = []
+dealerHand = []
+
 
 def clear():
     """
     Clear fuction for cleaning the terminal
     """
     # for windows
-    if name == 'nt':
-        _ = system('cls')
- 
+    if name == "nt":
+        _ = system("cls")
+
     # for mac and linux(here, os.name is 'posix')
     else:
-        _ = system('clear')
+        _ = system("clear")
 
-def sprint(str):
+
+def sprint(string):
     """
     Function for priting out text slowly
     """
-    for c in str + '\n':
-        sys.stdout.write(c)
+    for character in string + "\n":
+        sys.stdout.write(character)
         sys.stdout.flush()
-        time.sleep(3./90)
+        time.sleep(3.0 / 90)
+
+
+def deal_card(turn):
+    """
+    Deal Card function, selects a random card from the deck, appends it to
+    whoever turn it is, and removes this card from the deck
+    """
+    card = random.choice(deck)
+    turn.append(card)
+    deck.remove(card)
+
+
+def total(turn):
+    """
+    This function calculates de total based on what card it is
+    """
+    total = 0
+    face = ["J", "K", "Q"]
+    for card in turn:
+        if card in range(1, 11):
+            total += card
+        elif card in face:
+            total += 10
+        else:
+            if total > 11:
+                total += 1
+            else:
+                total += 10
+    return total
+
+
+def revealDealerHand():
+    """
+    This function reveals the dealer hand when needed, in case a card is added or in case someone wins.
+    """
+    if len(dealerHand) == 2:
+        return dealerHand[0]
+    elif len(dealerHand) >= 2:
+        return dealerHand[0], dealerHand[1]
+
 
 def main():
-    global playerScore, dealerScore
-    #Global variables for when players are playing or not
+    global PLAYER_SCORE, DEALER_SCORE
+    # Global variables for when players are playing or not
     playerIn = True
     dealerIn = True
 
-    # Setting Lists for the Deck, The player's Hand, and the Dealer's hand
-    deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'A', 'J', 'Q', 'K', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'A', 'J', 'Q', 'K', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'A', 'J', 'Q', 'K', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'A', 'J', 'Q', 'K']
-    playerHand = []
-    dealerHand = []
 
-    def dealCard(turn):
-        """
-        Deal Card function, selects a random card from the deck, appends it to
-        whoever turn it is, and removes this card from the deck
-        """
-        card = random.choice(deck)
-        turn.append(card)
-        deck.remove(card)
+    for _ in range(2):  # Give cards to both players
+        deal_card(playerHand)
+        deal_card(dealerHand)
 
-    def total(turn):
-        """
-        This function calculates de total based on what card it is
-        """
-        total = 0
-        face = ['J', 'K', 'Q']
-        for card in turn:
-            if card in range(1, 11):
-                total += card
-            elif card in face:
-                total += 10  
-            else:
-                if total > 11:
-                    total += 1 
-                else:
-                    total += 10
-        return total
-
-    def revealDealerHand():
-        """
-        This function reveals the dealer hand when needed, in case a card is added or in case someone wins.
-        """
-        if len(dealerHand) == 2:
-            return dealerHand[0]
-        elif len(dealerHand) >= 2:
-            return dealerHand[0], dealerHand[1]
-        
-    for _ in range(2): # Give cards to both players
-        dealCard(playerHand)
-        dealCard(dealerHand)
-
-    while playerIn or dealerIn: # Main game loop, gives card to players and dealer, break if it's >= 21
+    while (
+        playerIn or dealerIn
+    ):  # Main game loop, gives card to players and dealer, break if it's >= 21
         if len(dealerHand) <= 2:
             sprint(f"\nDealer has: {revealDealerHand()} and X")
         else:
-            sprint(f"\nDealer has: {dealerHand}, total of {total(dealerHand)} points.\n")
+            sprint(
+                f"\nDealer has: {dealerHand}, total of {total(dealerHand)} points.\n"
+            )
         sprint(f"{user} has: {playerHand}, total of {total(playerHand)} points\n")
         stayOrHit = input("1 to Stay\n2 to Hit\n")
-        while stayOrHit not in ['1', '2']: #If user has not typed either 1 or 2
+        while stayOrHit not in ["1", "2"]:  # If user has not typed either 1 or 2
             sprint("Invalid option. Please choose either 1 or 2.")
             stayOrHit = input("1 to Stay\n2 to Hit\n")
-    
+
         if total(dealerHand) > 17:
             sprint("Dealer stays")
             dealerIn = False
         else:
             sprint("Dealer hits and gets a new card")
-            dealCard(dealerHand)
+            deal_card(dealerHand)
 
-        if stayOrHit == '1':
+        if stayOrHit == "1":
             sprint(f"{user} stays")
             playerIn = False
         else:
             sprint(f"{user} hits and gets a new card")
-            dealCard(playerHand)
-    
-        if total(playerHand) >= 21: #Break statements in case someone wins or busts
+            deal_card(playerHand)
+
+        if total(playerHand) >= 21:  # Break statements in case someone wins or busts
             break
         elif total(dealerHand) >= 21:
             break
 
     # This if elif statements will check for the total of points in each hand and if someone wins they will stop the game and announce a winner
-    if total(playerHand) == 21: 
-        sprint(f"\nThe dealer has: {dealerHand}, Total of: {total(dealerHand)} points.\n{user} has: {playerHand}, Total of: {total(playerHand)} points.")
+    if total(playerHand) == 21:
+        sprint(
+            f"\nThe dealer has: {dealerHand}, Total of: {total(dealerHand)} points.\n{user} has: {playerHand}, Total of: {total(playerHand)} points."
+        )
         sprint(f"Blackjack! {user} wins!")
-        playerScore+= 1
+        PLAYER_SCORE += 1
     elif total(dealerHand) == 21:
-        sprint(f"\nThe dealer has: {dealerHand}, Total of: {total(dealerHand)} points.\n{user} has: {playerHand}, Total of: {total(playerHand)} points.")
+        sprint(
+            f"\nThe dealer has: {dealerHand}, Total of: {total(dealerHand)} points.\n{user} has: {playerHand}, Total of: {total(playerHand)} points."
+        )
         sprint(f"Blackjack! Dealer wins!")
-        dealerScore+= 1
+        DEALER_SCORE += 1
     elif total(playerHand) > 21:
-        sprint(f"\nThe dealer has: {dealerHand}, Total of: {total(dealerHand)} points.\n{user} has: {playerHand}, Total of: {total(playerHand)} points.")
+        sprint(
+            f"\nThe dealer has: {dealerHand}, Total of: {total(dealerHand)} points.\n{user} has: {playerHand}, Total of: {total(playerHand)} points."
+        )
         sprint(f"You bust! Dealer wins!")
-        dealerScore+= 1
+        DEALER_SCORE += 1
     elif total(dealerHand) > 21:
-        sprint(f"\nThe dealer has: {dealerHand}, Total of: {total(dealerHand)} points.\n{user} has: {playerHand}, Total of: {total(playerHand)} points.")
+        sprint(
+            f"\nThe dealer has: {dealerHand}, Total of: {total(dealerHand)} points.\n{user} has: {playerHand}, Total of: {total(playerHand)} points."
+        )
         sprint(f"Dealer Busts! {user} wins!")
-        playerScore+= 1
-    elif 21 - total (dealerHand) < 21 - total(playerHand):
-        sprint(f"\nThe dealer has: {dealerHand}, Total of: {total(dealerHand)} points.\n{user} has: {playerHand}, Total of: {total(playerHand)} points.")
+        PLAYER_SCORE += 1
+    elif 21 - total(dealerHand) < 21 - total(playerHand):
+        sprint(
+            f"\nThe dealer has: {dealerHand}, Total of: {total(dealerHand)} points.\n{user} has: {playerHand}, Total of: {total(playerHand)} points."
+        )
         sprint(f"Dealer wins")
-        dealerScore+= 1
-    elif 21 - total (dealerHand) > 21 - total(playerHand):
-        sprint(f"\nThe dealer has: {dealerHand}, Total of: {total(dealerHand)} points.\n{user} has: {playerHand}, Total of: {total(playerHand)} points.")
+        DEALER_SCORE += 1
+    elif 21 - total(dealerHand) > 21 - total(playerHand):
+        sprint(
+            f"\nThe dealer has: {dealerHand}, Total of: {total(dealerHand)} points.\n{user} has: {playerHand}, Total of: {total(playerHand)} points."
+        )
         sprint(f"{user} wins")
-        playerScore+= 1
-        
+        PLAYER_SCORE += 1
+
     def playAgain():
         """
         This function will start the game again in case the player wants so
         """
-        global playerScore, dealerScore
+        global PLAYER_SCORE, DEALER_SCORE
         play_again = str(input(f"Would you like to play again? Y/N\n").lower())
 
         if play_again[0] != "y":
             sleep(1)
             clear()
             sleep(1)
-            sprint(f"Alright. Thanks for playing with us! See you again another time.\nFinal Score: {user}: {playerScore}, Dealer: {dealerScore}")
+            sprint(
+                f"Alright. Thanks for playing with us! See you again another time.\nFinal Score: {user}: {PLAYER_SCORE}, Dealer: {DEALER_SCORE}"
+            )
             exit()
         else:
             sleep(1)
             clear()
             sleep(1)
-            sprint(f"Alright. A new game is starting.\nScore: {user}: {playerScore}, Dealer: {dealerScore}")
+            sprint(
+                f"Alright. A new game is starting.\nScore: {user}: {PLAYER_SCORE}, Dealer: {DEALER_SCORE}"
+            )
+            deck = generate_deck()
+            playerHand = []
+            dealerHand = []
             main()
 
     playAgain()
+
 
 # Game starting messages
 sprint("Welcome to Paddy's BlackJack!")
@@ -162,7 +258,9 @@ sprint("What is your name, our little winner?")
 user = input("")
 
 
-while True: # Offer the player to read the rules and restarts in case option is not valid (Y or N)
+while (
+    True
+):  # Offer the player to read the rules and restarts in case option is not valid (Y or N)
     sprint("Would you like to read the rules? Y/N")
     readRules = str(input("").lower())
 
@@ -172,11 +270,19 @@ while True: # Offer the player to read the rules and restarts in case option is 
         time.sleep(1)
         sprint("The rules are:")
         sprint("Blackjack is played with a conventional deck of 52 playing cards")
-        sprint("2 through 10 count at face value, i.e. a 2 counts as two, a 9 counts as nine.")
+        sprint(
+            "2 through 10 count at face value, i.e. a 2 counts as two, a 9 counts as nine."
+        )
         sprint("Face cards (J,Q,K) count as 10.")
-        sprint("Ace can count as a 1 or an 11 depending on which value helps the hand the most.")
-        sprint("The aim of blackjack is to finish the game with a higher total than that of the dealer, without exceeding 21.")
-        sprint("Going over 21 is commonly known as busting and means an automatic loss.")
+        sprint(
+            "Ace can count as a 1 or an 11 depending on which value helps the hand the most."
+        )
+        sprint(
+            "The aim of blackjack is to finish the game with a higher total than that of the dealer, without exceeding 21."
+        )
+        sprint(
+            "Going over 21 is commonly known as busting and means an automatic loss."
+        )
         sleep(3)
         break
     elif readRules and readRules[0] == "n":
@@ -184,15 +290,18 @@ while True: # Offer the player to read the rules and restarts in case option is 
         break
     else:
         sprint("Invalid option. Please choose either Y or N.")
-    
-    
-while True: #Offer the player to play a game and restarts in case option is not valid (Y or N)
+
+
+while (
+    True
+):  # Offer the player to play a game and restarts in case option is not valid (Y or N)
     playOrNo = input(f"Would you like to play a game? Y/N\n").lower()
 
     if playOrNo == "y":
         time.sleep(1)
         clear()
         time.sleep(1)
+        deck = generate_deck()
         main()
         break
     elif playOrNo == "n":
@@ -200,4 +309,3 @@ while True: #Offer the player to play a game and restarts in case option is not 
         break
     else:
         sprint("Invalid option. Please choose either Y or N.")
-                
